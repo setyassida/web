f4580bdf2058ada2d2a5ffca12bf8dd7bd573511
purinaqa <purinaqurota@gmail.com>
2018-02-14 22:37:03 +0700

    persiapan demo 1

---
+++ database/migrations/2018_02_14_152609_create_prestasi_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrestasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestasi', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('juara');
+            $table->string('nama_lomba');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestasi');
+    }
+}
